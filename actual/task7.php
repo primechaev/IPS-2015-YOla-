44c9cac92d3f59292917c609ef8e43a69a4eb689
primechaev <roman.primechaev@gmail.com>
2016-05-30 22:46:14 +0300

    Create task7.php

---
+++ actual/task7.php
@@ -0,0 +1,21 @@
+$arg1 = intval($_GET["arg1"]);
+$arg2 = intval($_GET["arg2"]);
+$operation = $_GET["operation"];
+switch ($operation)
+{
+    case "add":
+        $result = $arg1 + $arg2;
+        break;
+    case "sub":
+        $result = $arg1 - $arg2;
+        break;
+    case "mul":
+        $result = $arg1 * $arg2;
+        break;
+    case "div":
+        $result = $arg1/$arg2;
+        break;
+    default:
+        $result = "Error!";
+}
+echo "$arg1 $operation $arg2 = $result";
